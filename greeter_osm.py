446eea1ab1428358528552837b382b6886290b42
Miroslav Suchý <msuchy@redhat.com>
2020-12-10 18:40:06 +0100

    move history to separate file

--- greeter_osm.py
+++ greeter_osm.py
@@ -4,21 +4,6 @@
 greeter_osmsk.py 0.7
  skript, ktory sleduje newusers rss feed a ked objavi
  novacika editujuceho slovensko, posle mu uvitaciu spravu
-
-historia
- 0.1 - prvy "release"
- 0.2 - doplneny link - definicia changesetu
-     - odkaz na uvodne stranky pre zaciatocnikov
- 0.3 - moznost poslat jednorazovu spravu vybranemu uzivatelovi (specifikovanemu
-       ako prvy argument skriptu)
- 0.4 - skript prepisany do pythonu bez zavislosti na rsstail
-     - oddeleny kod a konfiguracia (.greeterrc)
- 0.5 - pridana moznost logovania do suboru (prepinac -l)
-     - upraveny sposob autentizacie
- 0.6 - PEP8
-     - neposielaj spravu o chybajucom source tagu pre changeset ak bol pouzity
-       editor iD
- 0.7 - oprava posielanie po zmene URL na osm
 """
 
 import argparse
